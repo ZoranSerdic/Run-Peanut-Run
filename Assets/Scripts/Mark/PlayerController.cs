b24186bdee1436336d420729b5427ced5b4fd521
ItsM3rk <mswaantje@gmail.com>
2019-02-04 14:32:15 +0100

    added PlayerPrefab with basic movement
    movement will be inproved

--- Assets/Scripts/Mark/PlayerController.cs
+++ Assets/Scripts/Mark/PlayerController.cs
@@ -5,31 +5,62 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    GameObject Player;
+    GameObject Model;
     [SerializeField]
     private string HorizontalControl = "Horizontal_P1";
     [SerializeField]
     private string VerticalControl = "Vertical_P1";
+    [SerializeField]
+    private string SpeedControl = "VerticalRight_P1";
+    private float roll;
+    private float rollRotation;
 
 
-    void Start ()
-    {
-		
-	}
-	
+    private float MaxSpeed = 0.001f;
+
+    private Vector3 Velocity;
+    private Vector3 dir;
+
 
 	void Update ()
     {
-        float h = Input.GetAxis(HorizontalControl) / 8;
-        if (h > 0.1f || h < -0.1f)
+        //rotation.z ( forward = negative ) 
+
+        float h = Input.GetAxis(HorizontalControl);
+        float j = Input.GetAxis(VerticalControl);
+
+        if (j > 0.001f || h > 0.001f)
         {
-            Player.transform.Translate(h, 0, 0);
+            dir = this.transform.position - new Vector3(-j, 0, -h);
+            this.transform.LookAt(dir);
         }
-        float j = Input.GetAxis(VerticalControl) / 8;
-        if (j > 0.1f || j < -0.1f)
+
+
+
+        float Speed = Input.GetAxis(SpeedControl);
+        if (Speed > 0.2 || Speed < 0.2)
         {
-            Player.transform.Translate(0,0,j * -1);
+            roll += Speed / 20 ;
+            rollRotation += Speed;
+
+            
+            transform.Translate(new Vector3(-roll, 0, 0) * Time.deltaTime);
+
         }
+        else
+        {
+            roll = 0;
+            rollRotation = 0;
+
+        }
+
+        if (roll > MaxSpeed)
+        {
+            roll = MaxSpeed;
+        }
+
+        //Model.transform.Rotate(0, rollRotation, 0);
+
 
     }
 }
