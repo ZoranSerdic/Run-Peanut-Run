eda4899e66a399e723a20874d711cf9ab07ec9b6
ItsM3rk <mswaantje@gmail.com>
2019-02-04 11:12:31 +0100

    Basic PlayerMovement
    added basic movement for the player, no velocity

--- Assets/Scripts/Mark/PlayerController.cs
+++ Assets/Scripts/Mark/PlayerController.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+    [SerializeField]
+    private string HorizontalControl = "Horizontal_P1";
+    [SerializeField]
+    private string VerticalControl = "Vertical_P1";
 
-	// Use this for initialization
-	void Start () {
+
+    void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	void Update ()
+    {
+        float h = Input.GetAxis(HorizontalControl) / 8;
+        if (h > 0.1f || h < -0.1f)
+        {
+            Player.transform.Translate(h, 0, 0);
+        }
+        float j = Input.GetAxis(VerticalControl) / 8;
+        if (j > 0.1f || j < -0.1f)
+        {
+            Player.transform.Translate(0,0,j * -1);
+        }
+
+    }
 }
