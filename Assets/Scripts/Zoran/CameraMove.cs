87a69a67e52dc0bf0863148d32c6d1214b098ab8
Unknown <zster1204@gmail.com>
2019-02-04 14:37:13 +0100

    Zoran Scene changed, Multiple models added

--- Assets/Scripts/Zoran/CameraMove.cs
+++ Assets/Scripts/Zoran/CameraMove.cs
@@ -4,11 +4,41 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    public float camMoveSpeed = 2f;
+    public float camStartMoveSpeed = 2f;
+    public float camMidMoveSpeed = 3f;
+    public float camEndMoveSpeed = 4f;
+
+    private float timer;
+
+    private bool timeUpStart = false;
+    private bool timeUpMid = false;
 
 	void Update ()
     {
+        timer += 1 * Time.deltaTime;
 
-        transform.Translate(Vector3.right * camMoveSpeed * Time.deltaTime);
+        if (timer >= 15)
+        {
+            timeUpStart = true;
+            Debug.Log("Time start is up");
+        }
+        if (timer >= 30)
+        {
+            timeUpMid = true;
+            Debug.Log("Time mid is up");
+        }
+
+        if (timeUpStart == true && timeUpMid == true)
+        {
+            transform.Translate(Vector3.right * camEndMoveSpeed * Time.deltaTime);
+        }
+        else if (timeUpStart == true)
+        {
+            transform.Translate(Vector3.right * camMidMoveSpeed * Time.deltaTime);
+        }
+        else if(timeUpStart == false)
+        {
+            transform.Translate(Vector3.right * camStartMoveSpeed * Time.deltaTime);
+        }
 	}
 }
