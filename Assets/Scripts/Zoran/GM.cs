e01a610dadd09c56512fb211cfcdd06e3c55425c
Unknown <zster1204@gmail.com>
2019-02-04 11:24:51 +0100

    RQ, Timer, Finish, RandomLevel system added to Zoran

--- Assets/Scripts/Zoran/GM.cs
+++ Assets/Scripts/Zoran/GM.cs
@@ -1,20 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
 
 public class GM : MonoBehaviour
 {
-    public TextMeshProUGUI realText;
+    public TextMeshProUGUI timerText;
 
-    private float realTime = 0.0f;
-    private float time;
+    private float realTime;
+    private float timer;
 
-    void Update()
+	void Start ()
     {
+		
+	}
+	
+	void Update ()
+    {
+        #region Time system
         realTime += UnityEngine.Time.deltaTime;
-        time = Mathf.Floor(realTime);
+        timer = Mathf.Floor(realTime);
 
-        realText.text = "" + time;
+        timerText.text = "" + timer;
+        #endregion
+
+        #region RQ system
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Debug.Log("Reloaded current scene");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+            Debug.Log("Application quit");
+        }
+        #endregion
     }
 }
---
+++ Assets/Scripts/Zoran/RandomLevel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomLevel : MonoBehaviour
+{
+    int randomLevel;
+
+    public GameObject Level_01;
+    public GameObject Level_02;
+    public GameObject Level_03;
+    public GameObject Level_04;
+    public GameObject Level_05;
+
+    void Awake()
+    {
+        randomLevel = Random.Range(1, 5);
+
+        Level_01.SetActive(false);
+        Level_02.SetActive(false);
+        Level_03.SetActive(false);
+        Level_04.SetActive(false);
+        Level_05.SetActive(false);
+    }
+
+    void Update()
+    {
+        switch (randomLevel)
+        {
+            default:
+                Level_01.SetActive(true);
+                break;
+
+            case 1:
+                Level_01.SetActive(true);
+                break;
+            case 2:
+                Level_02.SetActive(true);
+                break;
+            case 3:
+                Level_03.SetActive(true);
+                break;
+            case 4:
+                Level_04.SetActive(true);
+                break;
+            case 5:
+                Level_05.SetActive(true);
+                break;
+        }
+    }
+}
